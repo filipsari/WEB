b22d2831b2213a63f68a80daeca4b38ba20a0c92
FilipSari <filipsari@gmail.com>
2021-07-10 13:59:45 +0200

    Work in progress

--- BIT Show project/JS/app.js
+++ BIT Show project/JS/app.js
@@ -1,43 +1,80 @@
-const endpoint = "http://api.tvmaze.com/shows";
-let main = document.querySelector(".main-div");
-let top50 ={};
+window.addEventListener('load', function() {
+  window.localStorage.clear();
 
-const request = new XMLHttpRequest();
-request.open("GET", endpoint, true);
-request.onload = function(){
-  if (request.status === 200){
-    const data = JSON.parse(request.responseText);
-  
-   let sortedShow = data.sort( function(x,y){
-      return y.rating.average - x.rating.average;  
-   })
+  // const endpoint = "https://api.tvmaze.com/shows";
+  // const endpointSearch = "https://api.tvmaze.com/search/shows?q=:";
 
-   top50 = sortedShow.slice(0,50);
-   top50.forEach(function(element){
-    let mainDiv = document.createElement("div");
-    mainDiv.setAttribute("our-key", element.id);
-    mainDiv.classList = "tv-div";
-    let img = document.createElement("img");
-    img.setAttribute("src", element.image.medium);
-    let title = document.createElement("h4");
-    title.textContent = element.name;
-    main.append(mainDiv);
-    mainDiv.append(img);
-    mainDiv.append(title);
-   })
-   
-   window.localStorage.clear();
+  const baseUrl = 'https://api.tvmaze.com';
 
-   let div = document.querySelectorAll(".tv-div")
-   div.forEach(function(element){
-     element.addEventListener("click", function(event){
-       event.preventDefault();
-       let key = document.querySelector(".tv-div").getAttribute("our-key");
-       window.localStorage.setItem("id", key);
-       document.location = "second.html"
-     })
-   }) 
-  }};
+  let main = document.querySelector(".main-div");
+  let top50 =[];
 
+  const request = new XMLHttpRequest();
+  request.open("GET", `${baseUrl}/shows`, true);
+  request.onload = function(){
+    if (request.status === 200){
+      const data = JSON.parse(request.responseText);
+    
+    let sortedShow = data.sort( function(x,y){
+        return y.rating.average - x.rating.average;  
+    })
 
-request.send();
+    top50 = sortedShow.slice(0,50);
+    top50.forEach(function(element){
+      let mainDiv = document.createElement("div");
+      mainDiv.setAttribute("data-our-key", element.id);
+      mainDiv.classList = "tv-div";
+      let img = document.createElement("img");
+      img.setAttribute("src", element.image.medium);
+      let title = document.createElement("h4");
+      title.textContent = element.name;
+      main.append(mainDiv);
+      mainDiv.append(img);
+      mainDiv.append(title);
+    })
+    
+    //  window.localStorage.clear();
+
+    let div = document.querySelectorAll(".tv-div")
+    div.forEach(function(element){
+      element.addEventListener("click", function(event){
+     
+        let key = element.getAttribute("data-our-key");  
+     
+        console.log(key);
+        window.localStorage.setItem("id", key);
+        document.location = "second.html"
+      })
+    }) 
+    }};
+  request.send();
+
+  // const requestSearch = new XMLHttpRequest();
+  // requestSearch.open("GET", endpoint, true);
+  // requestSearch.onload = function(){
+  //   if (request.status === 200){
+  //     const data = JSON.parse(request.responseText);
+  //     console.log({data})
+  //   }
+  // }
+
+  const btnSearch = document.getElementById('btnSearch');
+  btnSearch.addEventListener('keyup', function(event) {
+    console.log(event)
+    if (!event.target.value) {
+      return;
+    }
+
+    const requestSearch = new XMLHttpRequest();
+    console.log({requestSearch})
+    requestSearch.open("GET", `${baseUrl}/search/shows?q=${event.target.value}`, true);
+    requestSearch.onload = function(){
+      if (requestSearch.status === 200){
+        const searchResults = JSON.parse(requestSearch.responseText);
+        console.log({searchResults})
+      }
+    }
+    requestSearch.send();
+  })
+})
+
--- BIT Show project/JS/app2.js
+++ BIT Show project/JS/app2.js
@@ -1,10 +1,112 @@
+window.addEventListener('load', function() {
+  // window.localStorage.clear();
+// TV MAZE API: /shows/:id/seasons(:id je dinamicki)
+// primer za detalje: https://api.tvmaze.com/shows/169
+// primer za sezone: https://api.tvmaze.com/shows/169/seasons
+// primer za glumce: https://api.tvmaze.com/shows/169/cast
+// broj sezona = duzina response-a sa /shows/:id/seasons (data.length)
 
-var key = window.localStorage.getItem("id");
-console.log(localStorage);
-request.open("GET", "http://api.tvmaze.com/shows/" + key + "/seasons");
+var key = window.localStorage.getItem("id");  
+//console.log(localStorage);
+const baseUrl = 'https://api.tvmaze.com';
+let main = document.querySelector(".container");
+let rightDiv = document.querySelector(".right")
 
-// first request for show 
+const requestSeasons = new XMLHttpRequest();
+requestSeasons.open("GET", "http://api.tvmaze.com/shows/" + key + "/seasons");
+//requestSeasons.open("GET", `${baseUrl}/shows/${key}/seasons`, true);
+// isto kao i gornji template string: requestSeasons.open("GET", baseUrl + "/shows/" + key + "/seasons")
+requestSeasons.onload = function(){
+  if (requestSeasons.status === 200){
+    const data = JSON.parse(requestSeasons.responseText);
 
-// second request for season 
+    
+      //  ******* creating Season (number) ***** 
+      let mainDiv = document.createElement("div");
+      let title = document.createElement("h3");
+      title.textContent = "Seasons (" + data.length + ")";
+      main.append(mainDiv);
+      mainDiv.append(title);
+      //  ******* creating Season (number) ***** 
 
-// third request for cast
+
+      //  ******* creating Season LIST ***** 
+    data.forEach(function(element){
+    let listOfSeasons = document.createElement("div");
+    let li = document.createElement("li");
+    let ul = document.createElement("ul"); 
+
+    let seasonTime = document.createElement("h4");
+    let seasonTable = seasonTime.textContent = (element.premiereDate + " --- "  + element.endDate);
+    ul.textContent = seasonTable; 
+
+    main.append(listOfSeasons);
+    listOfSeasons.append(ul);
+    ul.append(li);
+    //  ******* creating Season LIST ***** 
+    })
+  }  
+}
+requestSeasons.send();
+
+
+
+/***************  THE CAST ******************************** */
+const requestCast = new XMLHttpRequest();
+requestCast.open("GET", "https://api.tvmaze.com/shows/" + key + "/cast"); 
+// requestCast.open("GET", `${baseUrl}/shows/${key}/cast`)
+requestCast.onload = function(){
+  if (requestCast.status === 200){
+    const data = JSON.parse(requestCast.responseText);
+    let cast = document.createElement("h3");
+    cast.textContent = "Cast";
+    main.append(cast);
+
+    data.forEach(function(element){
+    let actorList = document.createElement("div");
+    let actorLi = document.createElement("li");
+    let actorUl = document.createElement("ul");
+    let actors = document.createElement("h4");
+    var actorNames = actors.textContent = element.person.name;
+    actorLi.textContent=actorNames;
+
+    main.append(actorList);
+    actorList.append(actorUl);
+    actorUl.append(actorLi);
+    })
+  }
+}
+requestCast.send();
+/***************  THE CAST ******************************** */
+
+
+
+
+/***************  SHOW DETAILS ******************************** */
+const requestDetails = new XMLHttpRequest();    //slika
+// requestDetails.open("GET", "http://api.tvmaze.com/shows/" + key);
+requestDetails.open("GET", `${baseUrl}/shows/${key}`)
+requestDetails.onload = function(){
+  if (requestDetails.status === 200){
+    const data = JSON.parse(requestDetails.responseText);
+    let show = document.createElement("h3");
+    show.textContent = "Show details";
+    let divParagraph = document.createElement("div");
+   
+    main.append(show);
+    
+    main.append(divParagraph);
+    
+    divParagraph.innerHTML=data.summary;
+  
+     
+  }
+}
+
+requestDetails.send();
+
+});
+
+// za svaki search result, kreirati <li> i u okviru njega <a href="second.html" data-our-key={searchResult.show.id}></a>, i zatim
+    // postupati identicno kao sa prvih 50 serija sa pocetne strane, klikom na seriju dohvatis njen id (iz data-our-key) i tu vrednost 
+    //     ubaciti u localStorage, da kada dodjes na second.html znas koje podatke da dohvatis
